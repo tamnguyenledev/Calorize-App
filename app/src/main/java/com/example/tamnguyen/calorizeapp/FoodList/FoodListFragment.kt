6eb5259eb4875677c472c82878fb9c140a4450e8
lhdung1997 <lhdung1997@gmail.com>
2017-12-27 00:22:20 +0700

    FoodList Package: Start implementing Food Searching

--- app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/FoodListFragment.kt
+++ app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/FoodListFragment.kt
@@ -4,6 +4,8 @@ package com.example.tamnguyen.calorizeapp.FoodList
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,6 +14,9 @@ import android.widget.Toast
 import com.example.tamnguyen.calorizeapp.R
 import com.google.firebase.database.DatabaseError
 import kotlinx.android.synthetic.main.food_list.*
+import kotlinx.android.synthetic.main.food_list_rv.*
+import kotlinx.android.synthetic.main.fragment_food_list.*
+import me.xdrop.fuzzywuzzy.FuzzySearch
 import java.util.*
 
 
@@ -39,7 +44,12 @@ class FoodListFragment : Fragment() {
 
         //Init RecyclerView
         rvDiaryFoodList.layoutManager = LinearLayoutManager(context!!)
+        //Load Foods From Database
+        loadFoods()
 
+
+    }
+    private fun loadFoods(){
         //Load foods into RecyclerView
         FoodDatabase.instance.getFoods(object: FoodDatabase.OnCompleteListener{
             override fun onSuccess(meals: List<Meal>) {
@@ -52,6 +62,19 @@ class FoodListFragment : Fragment() {
                 Toast.makeText(context!!,err.message,Toast.LENGTH_LONG).show()
             }
 
+        })
+    }
+    private fun searchFoodConfig(){
+        etSearchFood.addTextChangedListener(object: TextWatcher{
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
         })
 
     }
