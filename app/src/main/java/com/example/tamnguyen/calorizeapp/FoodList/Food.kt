0d6ec8f693677cd1596bcab9a060898cac1b8115
lhdung1997 <lhdung1997@gmail.com>
2017-12-27 19:54:36 +0700

    Food List: - Customized Food::class to fix errors with Parcelable and add some members

--- app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/Food.kt
+++ app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/Food.kt
@@ -4,26 +4,31 @@ import android.annotation.SuppressLint
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import java.util.*
+import kotlin.collections.HashMap
 
 /**
  * Created by hoangdung on 12/17/17.
  */
 @SuppressLint("ParcelCreator")
 @Parcelize
-public class Food : Parcelable{
-    public var amount: Long? = null;
-    public var calorie: Double? = null
-    public var carb: Double? = null
-    public var fat: Double? = null
-    public var protein: Double? = null
-    public var dayType: Map<String,Boolean>? = null
-    public var unit: String? = null
-    public var foodName: String? = null
-    public var type: String? = null
+data class Food(var amount: Long,
+           var calorie: Double,
+           var carb: Double,
+           var fat: Double,
+           var protein: Double,
+                var massPerUnit: Double,
+                var dayType: Map<String, Boolean>,
+           var unit: String,
+           var foodName: String,
+           var type: String,
+                var photoUrl: String
+            ) : Parcelable {
 
+    constructor(): this(0,0.toDouble(),0.toDouble(),0.toDouble(),0.toDouble(),0.toDouble(),HashMap<String,Boolean>(),"","","","")
     companion object {
         val BREAKFAST = "breakfast"
         val LUNCH = "lunch"
-        val DINNER= "dinner"
+        val DINNER = "dinner"
     }
+
 }
