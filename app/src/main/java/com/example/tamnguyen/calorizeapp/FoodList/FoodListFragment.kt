60a188cf7c204651e7a6b6ac809f073c3976c40f
lhdung1997 <lhdung1997@gmail.com>
2017-12-27 12:29:09 +0700

    FoodList Package: Finish Implementing Food Searching (Maybe there are some bugs)

--- app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/FoodListFragment.kt
+++ app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/FoodListFragment.kt
@@ -2,6 +2,7 @@ package com.example.tamnguyen.calorizeapp.FoodList
 
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.text.Editable
@@ -18,6 +19,7 @@ import kotlinx.android.synthetic.main.food_list_rv.*
 import kotlinx.android.synthetic.main.fragment_food_list.*
 import me.xdrop.fuzzywuzzy.FuzzySearch
 import java.util.*
+import kotlin.collections.ArrayList
 
 
 /**
@@ -33,6 +35,15 @@ class FoodListFragment : Fragment() {
     }
     lateinit var mAdapter: FoodListAdapter
     lateinit var listener: OnItemClickListener
+
+    /**
+     * Used for 1s (1000ms) delay after users finish inputting
+     */
+    val delay = 1000L
+    var runnable: Runnable? = null
+    val handler = Handler()
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
@@ -47,8 +58,13 @@ class FoodListFragment : Fragment() {
         //Load Foods From Database
         loadFoods()
 
+        searchFoodConfig()
 
     }
+
+    /**
+     * Wrapper method for loading foods from database
+     */
     private fun loadFoods(){
         //Load foods into RecyclerView
         FoodDatabase.instance.getFoods(object: FoodDatabase.OnCompleteListener{
@@ -64,18 +80,41 @@ class FoodListFragment : Fragment() {
 
         })
     }
+
+    /**
+     * Food Searching Configuration
+     */
     private fun searchFoodConfig(){
+
         etSearchFood.addTextChangedListener(object: TextWatcher{
-            override fun afterTextChanged(s: Editable?) {
-
+            override fun afterTextChanged(s: Editable) {
+                //After users finish typing, we should call query operation after delay time
+                runnable = Runnable {
+                    searchFoodImp(s.toString())
+                }
+                handler.postDelayed(runnable,delay)
             }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
             }
 
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                //When users keep typing, we should not call query operation
+                handler.removeCallbacks(runnable)
             }
         })
 
+    }
+
+    /**
+     * Food Searching Implementation
+     */
+    private fun searchFoodImp(query: String){
+        val newMeals = ArrayList<Meal>()
+        for(meal in mAdapter.mGroups){
+            newMeals.add((meal as Meal).suggestFood(query))
+        }
+        mAdapter = FoodListAdapter(context!!,newMeals)
+        mAdapter.notifyDataSetChanged()
     }
 }// Required empty public constructor
--- app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/Meal.kt
+++ app/src/main/java/com/example/tamnguyen/calorizeapp/FoodList/Meal.kt
@@ -2,9 +2,39 @@ package com.example.tamnguyen.calorizeapp.FoodList
 
 import com.thoughtbot.expandablerecyclerview.models.ExpandableGroup
 import kotlinx.android.parcel.Parcelize
+import me.xdrop.fuzzywuzzy.FuzzySearch
 
 /**
  * Created by hoangdung on 12/17/17.
  */
-public class Meal constructor(title: String, items: List<Food>) : ExpandableGroup<Food>(title, items) {
+public class Meal constructor(title: String,items: List<Food>) : ExpandableGroup<Food>(title, items) {
+    var foodNames: ArrayList<String>
+    init {
+        //Initilize Array of Food Names
+        foodNames = ArrayList<String>()
+        for(food in items)
+        {
+            foodNames.add(food.foodName!!)
+        }
+    }
+
+    /**
+     * Utility function used for food searching
+     * @param foodName: query string
+     * @return Meal: a subset of original Meal based on query string
+     */
+    fun suggestFood(foodName: String): Meal{
+        //Use fuzzy search to extract "text matching" points from Food Names
+        val suggests = FuzzySearch.extractAll(foodName,foodNames)
+        val foods = ArrayList<Food>()
+        //For each suggestion
+        for(i in suggests.indices){
+            //Score Cutoff Value = 50
+            if(suggests[i].score >= 50)
+            {
+                foods.add(items[i])
+            }
+        }
+        return Meal(title,foods)
+    }
 }
